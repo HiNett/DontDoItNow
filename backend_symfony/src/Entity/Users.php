459ca5cd698114f8864f230f2cb7bf1cdea80868
DELILLE Elone <147847949+HiNett@users.noreply.github.com>
2025-11-19 09:16:27 +0100

    Database Update

---
+++ backend_symfony/src/Entity/Users.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UsersRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UsersRepository::class)]
+class Users
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 16)]
+    private ?string $pseudo = null;
+
+    #[ORM\Column(length: 64)]
+    private ?string $Email = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $password = null;
+
+    #[ORM\Column(type: Types::SMALLINT)]
+    private ?int $role = null;
+
+    /**
+     * @var Collection<int, UsersTasks>
+     */
+    #[ORM\OneToMany(targetEntity: UsersTasks::class, mappedBy: 'user', orphanRemoval: true)]
+    private Collection $usersTasks;
+
+    public function __construct()
+    {
+        $this->usersTasks = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPseudo(): ?string
+    {
+        return $this->pseudo;
+    }
+
+    public function setPseudo(string $pseudo): static
+    {
+        $this->pseudo = $pseudo;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->Email;
+    }
+
+    public function setEmail(string $Email): static
+    {
+        $this->Email = $Email;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getRole(): ?int
+    {
+        return $this->role;
+    }
+
+    public function setRole(int $role): static
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, UsersTasks>
+     */
+    public function getUsersTasks(): Collection
+    {
+        return $this->usersTasks;
+    }
+
+    public function addUsersTask(UsersTasks $usersTask): static
+    {
+        if (!$this->usersTasks->contains($usersTask)) {
+            $this->usersTasks->add($usersTask);
+            $usersTask->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeUsersTask(UsersTasks $usersTask): static
+    {
+        if ($this->usersTasks->removeElement($usersTask)) {
+            // set the owning side to null (unless already changed)
+            if ($usersTask->getUser() === $this) {
+                $usersTask->setUser(null);
+            }
+        }
+
+        return $this;
+    }
+}
